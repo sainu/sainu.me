e9dcde609d5ed6e23886ce448d2bd3d57a026bf1
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-21 19:54:41 +0900

    Define data which resolvers return

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -35,120 +35,119 @@ const typeDefs = gql`
     score: Int
   }
 `
+const profile = {
+  familyNameKanji: "道祖",
+  givenNameKanji: "克理",
+  familyNameKana: "さいのう",
+  givenNameKana: "かつとし",
+  familyNameEn: "Saino",
+  givenNameEn: "Katsutoshi",
+  nickname: "sainu",
+  imageUrl: "/images/profileImage.jpg",
+  job: "Software Program Developer",
+  email: "katsutoshi.saino@gmail.com",
+  bio: "東京都出身のエンジニア。大学在学中にインターンや受託でWeb開発を経験。" +
+    "その後、個人開発を経て、お金の会社に就職。" +
+    "Railsでのサーバー開発から始まり、JSのFW(AngularやNuxt)を使ったフロントエンド開発、" +
+    "AWSでインフラの構築を経験してきました。",
+  location: "Tokyo",
+}
+
+const webLinks = [
+  {
+    title: "職務経歴",
+    url: "https://github.com/sainu/resume",
+  },
+  {
+    title: "ブログ",
+    url: "https://sainu.hatenablog.jp/",
+  },
+]
+
+const socialLinks = [
+  {
+    name: "GitHub",
+    url: "https://github.com/sainu",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/sainuio",
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/sainou.katsutoshi",
+  },
+  {
+    name: "Wantedly",
+    url: "https://www.wantedly.com/id/sainu",
+  },
+  {
+    name: "Qiita",
+    url: "https://qiita.com/sainu",
+  },
+]
+
+const skills = [
+  {
+    name: "TypeScript",
+    score: 50,
+  },
+  {
+    name: "Ruby",
+    score: 60,
+  },
+  {
+    name: "Go",
+    score: 40,
+  },
+  {
+    name: "Ruby on Rails",
+    score: 67,
+  },
+  {
+    name: "Vue.js",
+    score: 58,
+  },
+  {
+    name: "Nuxt.js",
+    score: 55,
+  },
+  {
+    name: "React.js",
+    score: 48,
+  },
+  {
+    name: "Next.js",
+    score: 45,
+  },
+  {
+    name: "MySQL",
+    score: 55,
+  },
+  {
+    name: "Redis",
+    score: 55,
+  },
+  {
+    name: "Docker",
+    score: 53,
+  },
+  {
+    name: "S3",
+    score: 53,
+  },
+  {
+    name: "EC2",
+    score: 53,
+  },
+]
 
 const resolvers = {
   Query: {
-    profile() {
-      return {
-        familyNameKanji: "道祖",
-        givenNameKanji: "克理",
-        familyNameKana: "さいのう",
-        givenNameKana: "かつとし",
-        familyNameEn: "Saino",
-        givenNameEn: "Katsutoshi",
-        nickname: "sainu",
-        imageUrl: "/images/profileImage.jpg",
-        job: "Software Program Developer",
-        email: "katsutoshi.saino@gmail.com",
-        bio: "東京都出身のエンジニア。大学在学中にインターンや受託でWeb開発を経験。" +
-          "その後、個人開発を経て、お金の会社に就職。" +
-          "Railsでのサーバー開発から始まり、JSのFW(AngularやNuxt)を使ったフロントエンド開発、" +
-          "AWSでインフラの構築を経験してきました。",
-        location: "Tokyo",
-      }
-    },
-    webLinks() {
-      return [
-        {
-          title: "職務経歴",
-          url: "https://github.com/sainu/resume",
-        },
-        {
-          title: "ブログ",
-          url: "https://sainu.hatenablog.jp/",
-        },
-      ]
-    },
-    socialLinks() {
-      return [
-        {
-          name: "GitHub",
-          url: "https://github.com/sainu",
-        },
-        {
-          name: "Twitter",
-          url: "https://twitter.com/sainuio",
-        },
-        {
-          name: "Facebook",
-          url: "https://www.facebook.com/sainou.katsutoshi",
-        },
-        {
-          name: "Wantedly",
-          url: "https://www.wantedly.com/id/sainu",
-        },
-        {
-          name: "Qiita",
-          url: "https://qiita.com/sainu",
-        },
-      ]
-    },
-    skills() {
-      return [
-        {
-          name: "TypeScript",
-          score: 50,
-        },
-        {
-          name: "Ruby",
-          score: 60,
-        },
-        {
-          name: "Go",
-          score: 40,
-        },
-        {
-          name: "Ruby on Rails",
-          score: 67,
-        },
-        {
-          name: "Vue.js",
-          score: 58,
-        },
-        {
-          name: "Nuxt.js",
-          score: 55,
-        },
-        {
-          name: "React.js",
-          score: 48,
-        },
-        {
-          name: "Next.js",
-          score: 45,
-        },
-        {
-          name: "MySQL",
-          score: 55,
-        },
-        {
-          name: "Redis",
-          score: 55,
-        },
-        {
-          name: "Docker",
-          score: 53,
-        },
-        {
-          name: "S3",
-          score: 53,
-        },
-        {
-          name: "EC2",
-          score: 53,
-        },
-      ]
-    },
+    profile: () => profile,
+    webLinks: () => webLinks,
+    socialLinks: () => socialLinks,
+    skills: () => skills,
   }
 }
 
