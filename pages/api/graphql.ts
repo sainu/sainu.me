4ca0a5123d2fc05852c1e32a74f076ff9221732f
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-22 10:52:55 +0900

    Mv schma.ts to pages/api/graphql.ts

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,29 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { NextApiRequest, NextApiResponse, PageConfig } from 'next'
-import { schemaWithResolvers } from './graphql/schema'
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { Resolvers } from "pages/api/graphql/generated/resolvers";
+import { join } from 'path'
+import { getProfile } from "lib/services/getProfile";
+import { getWebLinks } from "lib/services/getWebLinks";
+import { getSocialLinks } from "lib/services/getSocialLinks";
+import { getSkills } from "lib/services/getSkills";
+
+const resolvers: Resolvers = {
+  Query: {
+    profile: () => getProfile(),
+    webLinks: () => getWebLinks(),
+    socialLinks: () => getSocialLinks(),
+    skills: () => getSkills(),
+  }
+}
+
+const schema = loadSchemaSync(join(__dirname, '../../../../schema.graphql'), {
+  loaders: [new GraphQLFileLoader]
+})
+
+export const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
 
 const apolloServer = new ApolloServer({ schema: schemaWithResolvers })
 
