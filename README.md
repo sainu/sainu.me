484654164d9fd7cec84d642996795272d1109f7d
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-22 10:18:54 +0900

    Generate resolvers from schema.graphql

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sainu's profile
+
+# Development
+
+## GraphQL API
+
+### Generate resolvers from schema.graphql
+
+```
+npx gql-gen
+```
+
+or
+
+```
+npx gql-gen --watch
+```
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { NextApiRequest, NextApiResponse, PageConfig } from 'next'
-import { schemaWithResolvers } from '../../graphql/schema'
+import { schemaWithResolvers } from 'lib/graphql/schema'
 
 const apolloServer = new ApolloServer({ schema: schemaWithResolvers })
 
