bc2459b6b15c1dbf610d4f0f2c91deabc49fa4fc
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-23 22:36:11 +0900

    Use environment variables

--- lib/httpClient.ts
+++ lib/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.API_BASE_URL,
   timeout: 3 * 60 * 1000,
   headers: {
     'User-Agent': 'profile-front'
