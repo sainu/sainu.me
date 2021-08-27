d76f600c4ff73b5a291c31aeda32d0f717f1f2a5
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-27 18:19:10 +0900

    Fix to call tailwind css class

--- components/SkillListItem.tsx
+++ components/SkillListItem.tsx
@@ -5,27 +5,27 @@ type Props = {
   skill: Skill
 }
 
-const colors = [
-  [50, 'text-gray-900'],
-  [100, 'text-gray-900'],
-  [200, 'text-gray-900'],
-  [300, 'text-gray-900'],
-  [400, 'text-white'],
-  [500, 'text-white'],
-  [600, 'text-white'],
-  [700, 'text-white'],
-  [800, 'text-white'],
-  [900, 'text-white'],
-]
-const colorsSize = colors.length
-
 export const SkillListItem: FC<Props> = ({ skill }) => {
+  const colors = [
+    ['bg-blue-50', 'text-gray-900'],
+    ['bg-blue-100', 'text-gray-900'],
+    ['bg-blue-200', 'text-gray-900'],
+    ['bg-blue-300', 'text-gray-900'],
+    ['bg-blue-400', 'text-white'],
+    ['bg-blue-500', 'text-white'],
+    ['bg-blue-600', 'text-white'],
+    ['bg-blue-700', 'text-white'],
+    ['bg-blue-800', 'text-white'],
+    ['bg-blue-900', 'text-white'],
+  ]
+  const colorsSize = colors.length
+
   const level = Math.floor(skill.score / colorsSize)
 
   return (
     <div className="bg-gray-100 rounded-full">
       <div style={{ width: `${skill.score}%` }}>
-        <div className={`animate-left-to-right flex items-center justify-start h-6 px-4 rounded-full bg-blue-${colors[level][0]}`}>
+        <div className={`animate-left-to-right flex items-center justify-start h-6 px-4 rounded-full ${colors[level][0]}`}>
           <span className={`${colors[level][1]} text-sm`}>
             {skill.name}
           </span>
