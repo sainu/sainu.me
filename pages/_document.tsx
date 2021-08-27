8c2c2256040edbfa1540d88eaed75e1f4cf2eb3a
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-27 09:07:34 +0900

    Dynamically set prefix attribute to head html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,38 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
+class MyDocument extends Document<{ prefix: string | undefined }> {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
 
-    return initialProps
+    const prefix = []
+    if (this.useOgp(ctx.pathname)) {
+      prefix.push('og: http://ogp.me/ns#')
+      if (ctx.pathname == '/') prefix.push('website: http://ogp.me/ns/website#')
+      if (ctx.pathname == '/about_me') prefix.push('profile: http://ogp.me/ns/profile#')
+      else prefix.push('article: http://ogp.me/ns/article#')
+    }
+
+    return {
+      ...initialProps,
+      prefix: prefix.length > 0 ? prefix.join(' ') : undefined
+    }
+  }
+
+  private static useOgp(pathname: string): boolean {
+    switch (pathname) {
+    case '/404':
+      return false
+    default:
+      return true
+    }
   }
 
   render() {
+    const prefix = this.props.prefix
+
     return (
       <Html lang="ja">
-        <Head prefix="og: http://ogp.me/ns# profile: http://ogp.me/ns/profile#">
+        <Head prefix={prefix}>
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <meta property="og:locale" content="ja_JP" />
