38e9eee0bb33844127a99c6654a40e48eaa0a462
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-27 18:25:01 +0900

    Add left-to-right animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'left-to-right': 'left-to-right 1s',
+      },
+      keyframes: {
+        'left-to-right': {
+          '0%': {
+            width: '0%',
+          },
+          '100%': {
+            width: '100%',
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
