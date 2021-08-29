ce8bfafbdabdbabd0c22c6e8a2c2e7c9bb9daf89
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-29 22:31:03 +0900

    Fix StaticPageSection css

--- components/StaticPageTitle.tsx
+++ components/StaticPageTitle.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const StaticPageTitle: FC = ({ children }) => {
   return (
-    <h1 className="text-3xl font-bold">
+    <h1 className="text-3xl font-bold mb-12">
       {children}
     </h1>
   )
